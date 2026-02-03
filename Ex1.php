6b237c7e8b374011ca3b1b7b57d0962485fedcde
aubin241 <diego.aubin@etu.unicaen.fr>
2026-02-03 17:33:10 +0100

    Ajout de l'affichage de la date dans Ex1.php

--- Ex1.php
+++ Ex1.php
@@ -1,10 +1,11 @@
 <!DOCTYPE html>
 <html>
 <head>
- <title></title>
+ <title>Exercice 1</title>
  <link rel="stylesheet" type="text/css" href="styles.css">
 </head>
 <body>
  <h1>Mon fichier HTML</h1>
+ <p>Aujourd'hui nous sommes le : <?php echo date('d/m/Y'); ?></p>
 </body>
 </html>
